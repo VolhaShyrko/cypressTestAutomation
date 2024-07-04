d22fba0eccfd06a04dafd23fcea695c9ff987e46
Olga Shyrko <olga.shyrko@bequalified.de>
2024-07-04 17:05:48 +0200

    add git ignore file

--- cypress/e2e/apiTests.cy.js
+++ cypress/e2e/apiTests.cy.js
@@ -5,7 +5,7 @@ describe('Login page', () => {
         });
     });
 
-    it('Check Login page is loaded with Status Code 200 # 2', () => {
+    it('Check Login page is loaded with Status Code 200 #2', () => {
         cy.request('GET', 'https://opensource-demo.orangehrmlive.com/').as('response');
         cy.get('@response').its('status').should('eq', 200);
     });
